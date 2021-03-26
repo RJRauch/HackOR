66270476bce6c3e71f6fe48aaca37232d3b25890
nsapse <nsapse@gmail.com>
2021-03-26 14:50:40 -0400

    added skeleton file

---
+++ main.py
@@ -0,0 +1,19 @@
+#################################################################################################
+#                                                                                               #
+#       Batch Editor (insert fun name here) - a Python Program Written for HackOR 2021          #
+#       Written by:                                                                             #
+#           Sophia L                                                                            #
+#           Ryan R.                                                                             #
+#           Noah S.                                                                             #
+#           Rain_Bow                                                                            #
+#                                                                                               #
+#################################################################################################
+
+# necessary libraries
+import os           # for file navigation
+import docx         # for manipulating .docX files
+import click        # for creating the UI
+
+
+if __name__ == "__main__":
+    pass
